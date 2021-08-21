8f8e3e6cb2996a806dd21b722652026faee7996a
Roland7013 <71085060+Roland7013@users.noreply.github.com>
2021-08-20 21:17:48 -0500

    Background

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public SpriteRenderer square;
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        square = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+     if (Time.time > nextActionTime ) {
+            nextActionTime += period;
+            // execute block of code here
+            square.color = Color.green;
+        }
+ 
+    
+    }
+}
